2820bb8e69a185c9a2b7f8af24842064fc1e48a4
Avinash Vyas <avinash.vyas@nokia.com>
2018-08-02 16:10:31 -0400

    First commit for identifying search and rewind

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
 
@@ -6,10 +5,32 @@ class YoutubeControllerSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder().require('YoutubeController'))
-    def handle_youtube_controller(self, message):
-        self.speak_dialog('youtube.controller')
+    def initialize(self):
+        self.register_rasa_intent('search.json', self.handle_youtube_search)
+        self.register_rasa_intent('rewind.json', self.handle_rewind)
 
+    def handle_youtube_search(self, message):
+        query = message.data.get("query")
+        if query is None:
+            self.speak_dialog('no.query')
+            return
+        resp = {'query': query}
+        self.speak_dialog('search.confirm', data=resp)
+
+    def handle_rewind(self, message):
+        time = messge.data.get("time")
+        if time is None:
+            self.speak_dialog('no.time')
+            return
+        if (time == 'start')
+            self.speak_dialog("Restarting the audio from the beginning")
+            return
+        unit = message.data.get("unit")
+        if unit is None:
+            self.speak_dialog('no.unit')
+            return
+        resp = {'time': time, 'unit' : unit}
+        self.speak_dialog('rewind.confirm', data=resp)
 
 def create_skill():
     return YoutubeControllerSkill()
