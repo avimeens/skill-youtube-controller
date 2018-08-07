8ac855b2bfe1fe77df347d7622d3a349104058e7
Avinash Vyas <avinash.vyas@nokia.com>
2018-08-07 14:36:43 -0400

    Added code for playing audio

--- __init__.py
+++ __init__.py
@@ -4,6 +4,9 @@ from mycroft import MycroftSkill, intent_handler
 class YoutubeControllerSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self._p = None
+        self._start = None
+        self._uri = None
 
     def initialize(self):
         self.register_rasa_intent('search.json', self.handle_youtube_search)
@@ -16,6 +19,8 @@ class YoutubeControllerSkill(MycroftSkill):
             return
         resp = {'query': query}
         self.speak_dialog('search.confirm', data=resp)
+        # download the file first
+        self.play("air-filter.wav")
 
     def handle_rewind(self, message):
         time = messge.data.get("time")
@@ -31,6 +36,52 @@ class YoutubeControllerSkill(MycroftSkill):
             return
         resp = {'time': time, 'unit' : unit}
         self.speak_dialog('rewind.confirm', data=resp)
+
+    def sox_fmt(seconds):
+        # sox format hh:mm:ss.frac
+        # h = int(seconds // 3600)
+        # m = int((seconds // 60) % 60)
+        # s = seconds % 60
+        out = '{:02d}:{:02d}:{:f}'.format(int(seconds // 3600),
+                                          int((seconds // 60) % 60), 
+                                          seconds % 60) 
+        return out
+
+    def play(self, file1, start=0, stop=0):
+        '''Play a portion of a file given start and
+        stop locations in fractional seconds.'''
+        self._uri = file1
+        self.jump_to(start, stop)
+
+    def jump_to(self, start, stop=0):
+        '''Play a portion of a file given start and
+        stop locations in fractional seconds.'''
+        if start < 0 or stop < 0:
+            raise ValueError('negative start or stop')
+        self.stop()
+        args = ['play', self._uri, 'trim', sox_fmt(start)]
+        if stop > 0:
+            args.append('=' + sox_fmt(stop)) 
+        self._p = Popen(args) 
+        self._start = monotonic() - start
+
+    def jump(self, seconds, duration=0):
+        '''Play a portion of a file given an +/- offset
+        and duration in fractional seconds.'''
+        if duration < 0:
+            raise ValueError('negative duration')
+        at = monotonic() - self._start
+        to = at + seconds
+        if to < 0:
+            to = 0
+        self.jump_to(to, to + duration if duration else 0)
+
+    def stop(self):
+        if self._p:
+            if self._p.returncode is None:
+                self._p.terminate()
+            self._p = None
+        pass
 
 def create_skill():
     return YoutubeControllerSkill()
